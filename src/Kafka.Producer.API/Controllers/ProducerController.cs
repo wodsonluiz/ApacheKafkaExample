c2e3dec736b7168ecc15ea7c09ff7f40b2fc1fb2
Wodson Correia <wodson.correia@bemobi.com.br>
2020-06-02 22:20:52 -0300

    commit initial

---
+++ src/Kafka.Producer.API/Controllers/ProducerController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Kafka.Producer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProducerController : ControllerBase
+    {
+        private readonly ILogger<ProducerController> _logger;
+
+        [HttpPost]
+        [ProducesResponseType(typeof(string), 201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        public IActionResult Post([FromQuery] string msg)
+        {
+            return Created("", SendMessageByKafka(msg));
+        }
+
+        public ProducerController(ILogger<ProducerController> logger)
+        {
+            _logger = logger;
+        }
+
+        private string SendMessageByKafka(string message)
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var sendResult = producer
+                                        .ProduceAsync("fila_pedido", new Message<Null, string> { Value = message })
+                                            .GetAwaiter()
+                                                .GetResult();
+
+                    _logger.LogInformation($"Mensagem '{sendResult.Value}' de '{sendResult.TopicPartitionOffset}' mensagem produzida");
+
+                    return $"Mensagem '{sendResult.Value}' de '{sendResult.TopicPartitionOffset}'";
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
